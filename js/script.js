9d689867b8b94b3bbde01f71657b5dcba39a1639
Patrick Baldwin <themanbehind@thepatrickalan.com>
2012-12-21 01:01:51 -0800

    Volume controls are working, except on reloading the page.

--- js/script.js
+++ js/script.js
@@ -6,18 +6,18 @@ jQuery(document).ready(function($) {
 	// Stuff to do as soon as the DOM is ready. Use $() w/o colliding with other libs;
 	var $tracks = $('.track'),
 		$playbackControls = $('.control-button'),
-		$volumeControls = $('#masterVolume'),
-		currentTime;
+		$volumeControls = $('.volume'),
+		masterVolume = 0,
+		currentTime = 0;
 
 	// Sync time across all tracks.
 	function syncTime() {
 		for (i = 0; i < $tracks.length; i++) {
 			$tracks[i].currentTime = currentTime;
 		}
-		//$tracks.currentTime = currentTime;
 	}
 
-	function playbackTrigger(control) {
+	function playbackControl(control) {
 
 		if (control === "pause") {
 			currentTime = $tracks[0].currentTime;
@@ -28,18 +28,48 @@ jQuery(document).ready(function($) {
 			control = "pause";
 		}
 
-		$tracks.trigger(control);
-
 		syncTime();
+
+		$tracks.trigger(control);
+	}
+
+	function volumeControl(control, level) {
+		if (control === "master") {
+			for (i = 0; i < $tracks.length; i++) {
+				var trackLevel = $volumeControls.eq(i).attr('value') / 100 - masterVolume;
+				if ( trackLevel > 0 ) {
+					$tracks[i].volume = trackLevel;
+				} else {
+					$tracks[i].volume = 0;
+				}
+			} 
+		} else {
+			var trackNumber = control.slice(5,6) - 1,
+			level = level / 100 - masterVolume;
+			if (level > 0) {
+				$tracks[trackNumber].volume = level;
+			} else {
+				$tracks[trackNumber].volume = 0;
+			}
+		}
 	}
 
 	$playbackControls.click( function() {
 		var controlName = $(this).attr('rel');
-		playbackTrigger(controlName);
+		playbackControl(controlName);
 	});
 
-	$volumeControls.change( function(){
-		var controlName = $(this).attr('name');
+	$volumeControls.change( function() {
+		var $this = $(this),
+			controlName = $this.attr('name'),
+			controlLevel = $this.attr('value');
+
+		if (controlName === "master") {
+			masterVolume = Math.abs(controlLevel / 100);
+		}
+
+		volumeControl(controlName, controlLevel);
+		console.log(masterVolume);
 	});
 
 });
